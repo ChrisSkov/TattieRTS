00a91e05c5c3b42b31cbfa8b295acdfc80b596e5
Chris Skov <stofferdyr@gmail.com>
2021-11-08 15:45:35 +0100

    start lesson 15

---
+++ RealTattieStrat/Assets/Drawme.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawme : MonoBehaviour
+{
+    public float radius = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(gameObject.transform.position, radius);
+    }
+}
