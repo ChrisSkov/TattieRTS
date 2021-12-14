c4b59e48a5fcd40938133337b63d6d206952b36e
Chris Skov <stofferdyr@gmail.com>
2021-12-14 20:40:51 +0100

    Start lesson 19

--- RealTattieStrat/Assets/Scripts/Units/UnitSelectionHandler.cs
+++ RealTattieStrat/Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -8,7 +8,7 @@ public class UnitSelectionHandler : MonoBehaviour
 {
     [SerializeField] private LayerMask layerMask = new LayerMask();
     private Camera mainCamera;
-    private List<Unit> selectedUnits = new List<Unit>();
+    public List<Unit> SelectedUnits {get;} = new List<Unit>();
     private void Start()
     {
         mainCamera = Camera.main;
@@ -18,11 +18,11 @@ public class UnitSelectionHandler : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            foreach (Unit selectedUnit in selectedUnits)
+            foreach (Unit selectedUnit in SelectedUnits)
             {
                 selectedUnit.Deselect();
             }
-            selectedUnits.Clear();
+            SelectedUnits.Clear();
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
@@ -38,9 +38,9 @@ public class UnitSelectionHandler : MonoBehaviour
         if (!hit.collider.TryGetComponent<Unit>(out Unit unit)) return;
         if (!unit.hasAuthority) return;
 
-        selectedUnits.Add(unit);
+        SelectedUnits.Add(unit);
 
-        foreach (Unit selectedUnit in selectedUnits)
+        foreach (Unit selectedUnit in SelectedUnits)
         {
             selectedUnit.Select();
         }
