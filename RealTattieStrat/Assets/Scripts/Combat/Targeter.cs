4cf7a7e697a3466b75278e3e833ca6fbc4b683a3
Chris Skov <stofferdyr@gmail.com>
2021-12-15 16:23:02 +0100

    Start lesson 23

---
+++ RealTattieStrat/Assets/Scripts/Combat/Targeter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Targeter : NetworkBehaviour
+{
+    [SerializeField] private Targetable target;
+
+    #region Server
+    [Command]
+    public void CMDSetTarget(GameObject targetGameObject)
+    {
+        if (!targetGameObject.TryGetComponent<Targetable>(out Targetable newTarget)) return;
+        target = newTarget;
+    }
+
+    [Server]
+    public void ClearTarget()
+    {
+        target = null;
+    }
+
+    #endregion 
+
+    #region Client
+
+
+    #endregion 
+}
--- RealTattieStrat/Assets/Scripts/Units/UnitMovement.cs
+++ RealTattieStrat/Assets/Scripts/Units/UnitMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 public class UnitMovement : NetworkBehaviour
 {
     [SerializeField] NavMeshAgent agent = null;
-
+    [SerializeField] private Targeter targeter = null;
 
     #region Server
 
@@ -21,6 +21,7 @@ public class UnitMovement : NetworkBehaviour
     [Command]
     public void CmdMove(Vector3 position)
     {
+        targeter.ClearTarget();
         if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) return;
         agent.SetDestination(hit.position);
 
